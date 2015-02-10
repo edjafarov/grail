12f75c2d57bcfd54b561e888cc45e57ecd4abce4
Eldar Djafarov <djkojb@gmail.com>
2015-02-10 21:24:55 +0200

    up basic tests

--- tests/grailjs.basic.spec.js
+++ tests/grailjs.basic.spec.js
@@ -21,14 +21,26 @@ describe('Bootstrap Grail app', function(){
         );
     }
   });
+
+  var ItemsComp = React.createClass({
+    render: function () {
+      return (
+          React.createElement("div", null, "Items")
+        );
+    }
+  });
   var rootSpy = sinon.spy();
   var nestedSpy = sinon.spy();
   var rootAction = PromisePipe().then(function(){
     rootSpy.apply(this, arguments)
   })
+  var nestedAction = PromisePipe().then(function(){
+    nestedSpy.apply(this, arguments)
+  })
+
 
   var routes = React.createElement(Route, {path: "/", handler: AppComp, action: rootAction}, 
-      React.createElement(Route, {name: "items", path: "items", handler: AppComp, action: nestedSpy})
+      React.createElement(Route, {name: "items", path: "items", handler: ItemsComp, action: nestedAction})
   )
 
   app.useRoutes(routes);
@@ -39,8 +51,21 @@ describe('Bootstrap Grail app', function(){
     var htmlSpy = sinon.spy();
     mokedApp.middleware({originalUrl: "/"},{end:function(htmlResult){
       expect(rootSpy.getCall(0).args[1].path).to.be.equal('/');
+      expect(nestedSpy.called).to.not.be.ok;
       expect(htmlResult).to.have.string('<div class="container"');
       expect(htmlResult).to.have.string('<!DOCTYPE html><html><head>');
+      expect(htmlResult).to.not.have.string('Items</div></div>');
+      done();
+    }});
+  })
+
+  it('the /items path should be rendered', function(done){
+    rootSpy.reset();
+    mokedApp.middleware({originalUrl: "/items"},{end:function(htmlResult){
+      expect(rootSpy.getCall(0).args[1].path).to.be.equal('/items');
+      expect(htmlResult).to.have.string('<div class="container"');
+      expect(htmlResult).to.have.string('<!DOCTYPE html><html><head>');
+      expect(htmlResult).to.have.string('Items</div></div>');
       done();
     }});
   })
